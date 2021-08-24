aba2d915c1e6c702ab4401926912ac364dd91ba2
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-24 20:05:29 +0100

    Update index.c

--- index.c
+++ index.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <time.h>
 char randomChunk(){
-        return rand() % 2 ? '#' : '-';                    
+        int r = rand();
+        return r % 2 ? '#' : '-';
 }
 void generateRandomMap(char map[5][13]){
         for(int i = 0; i < 4; i++){
                 for(int j = 0; j < 11;j++){
                         map[i][j] = randomChunk();
-		}
-	}
+                }
+        }
 }
 void updateScene(int *coordinates, char map[5][13], char updateCode)
 {
-	struct winsize size;
+        struct winsize size;
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
         if(size.ws_col < 12 || size.ws_row < 4)
         {
-                printf("Your terminal window is to sm
-all please enlarge it\n");
+                printf("Your terminal window is to small please enlarge it\n");
                 return;
         }
         static int score;
@@ -112,11 +113,10 @@ all please enlarge it\n");
         if(coordinates[0] == 0 && coordinates[1] == 0 || coordinates[0] == 0 && coordinates[1] == 11 || coordinates[0] == 4 && coordinates[1] == 0 || coordinates[0] == 4 && coordinates[1] == 11 )
         {
                 printf("\033[1;31mGame Over!\n\033[0mYour Score: %d",score);
-                printf("%s","\n");
+                printf("\n");
                 return;
         }
-        printf("Score: ");
-        printf("%d",score);
+        printf("Score: %d",score);
         printf("\n");
         for (int i = 0; i < 5; i++)
         {
@@ -128,6 +128,7 @@ all please enlarge it\n");
 }
 int main(int argc, char *argv[])
 {
+        srand(time(NULL));
         int coordinates[2] = {2, 6};
         char map[5][13] = {
                 {"############"},
