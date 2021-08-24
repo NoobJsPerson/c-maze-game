b96f50ecbd1ec2f34a395867d5aecc49af497df1
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-08-24 23:40:02 +0100

    Update index.c

--- index.c
+++ index.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <sys/ioctl.h>                               #include <unistd.h>                                  #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
 char randomChunk(){
         int r = rand();
         return r % 2 ? '#' : '-';
 }
 void generateRandomMap(char map[5][13]){
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < 5; i++){
                 for(int j = 0; j < 11;j++){
                         map[i][j] = randomChunk();
                 }
@@ -111,19 +113,33 @@ void updateScene(int *coordinates, char map[5][13], char updateCode)
         if(coordinates[0] == 0 && coordinates[1] == 0 || coordinates[0] == 0 && coordinates[1] == 11 || coordinates[0] == 4 && coordinates[1] == 0 || coordinates[0] == 4 && coordinates[1] == 11 )
         {
                 printf("\033[1;31mGame Over!\n\033[0mYour Score: %d",score);
-                printf("\nRestart: r, Leave: l\n");
+                printf("\nRestart: r, Leave: any other character\n");
                 char b[1];
                 scanf("%s",b);
                 switch(*b)
                 {
                         case 'r':
-                                score = 0;
-                                main();
-                                return;
-                        case 'l':
+                                score =  0;
+                                *action = '0';
+                                coordinates[0] = 2;
+                                coordinates[1] = 6;
+                                char defaultMap[5][13] = {
+                                {"############"},
+                                {"#----------#"},
+                                {"#-----*-----"},
+                                {"#----------#"},
+                                {"############"}};
+                                for(int i = 0; i < 5; i++)
+                                {
+                                        for(int j = 0; j < 11; j++)
+                                        {
+                                                map[i][j] = defaultMap[i][j];
+                                        }
+                                }
+                                break;
+                        default:
                                 return;
                 }
-                return;
         }
         printf("Score: %d",score);
         printf("\n");
